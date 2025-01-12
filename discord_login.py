ec87620192ed468f64a57381532158b6598b2b16
max <m.peklov@gmail.com>
2025-01-12 01:25:31 +0100

    Убрал файл с паролями

--- discord_login.py
+++ discord_login.py
@@ -1,7 +1,9 @@
 from playwright.sync_api import sync_playwright
-from loginDate import DISCORD_EMAIL, DISCORD_PASSWORD  # Импорт данных авторизации
 from load_config import HEADLESS_MODE
 import json
+
+DISCORD_EMAIL = input('Ведите эмейл для входа в Discord: ')
+DISCORD_PASSWORD = input('Ведите палроль для входа в Discord: ')
 
 def save_session(context, session_file="session.json"):
     # Сохранение состояния контекста (cookies и localStorage) в файл
--- loginDate.py
+++
@@ -1,2 +0,0 @@
-DISCORD_EMAIL = "login@gmail.com"  # Замените на ваш email
-DISCORD_PASSWORD = "password"  # Замените на ваш пароль
